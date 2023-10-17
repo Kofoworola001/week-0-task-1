1a25b1319e647d869f0c9d945720367efbcda56d
kofoworolao11@gmail.com <kofoworolao11@gmail.com>
2023-10-17 08:07:36 +0100

    week 0 task

--- algorithms/challenge-6.js
+++ algorithms/challenge-6.js
@@ -1,3 +1,14 @@
-function fizzBuzz(num) {}
+function fizzBuzz(num) {
+    if(num % 5 === 0 && num % 3 === 0) {
+        return "fizzbuzz";
+
+    } else if (num % 3 === 0) {
+        return "fizz";
+    } else if ( num % 5 === 0){
+        return "buzz"
+    } else {
+        return num
+    }
+}
 
 module.exports = fizzBuzz;
